601fb54b83af8ae59ed5951dbc7f7f5f67885ce3
Tukain <tucano@firemail.cc>
2025-12-22 16:47:28 -0300

    Updates

--- blog.sh
+++ blog.sh
@@ -136,14 +136,12 @@ build_site() {
     OUT_FILE="$OUT_DIR/index.html"
     mkdir -p $OUT_DIR
     cat ./pages/head.html   >  $OUT_FILE
-    cat ./pages/navbar.html >> $OUT_FILE
-    printf "<main>
-            <time>
-    $POST_DAY/$POST_MONTH/$POST_YEAR
-            </time>"        >> $OUT_FILE
-    smu ./content/"$FILE" | sed 's/<a /<a target="_blank" /g' >> $OUT_FILE
-    printf "</main>"        >> $OUT_FILE
-    cat ./pages/footer.html >> $OUT_FILE
+    [ -f "./pages/navbar.html" ] && cat ./pages/navbar.html >> $OUT_FILE
+    echo "<main>
+            <time>$POST_DAY/$POST_MONTH/$POST_YEAR</time>
+            $(smu ./content/"$FILE" | sed 's/<a /<a target="_blank" /g')
+            </main>"        >> $OUT_FILE
+    [ -f "./pages/footer.html" ] && cat ./pages/footer.html >> $OUT_FILE
   done
 
   BLOG_OUTPUT="$BLOG_DIR/index.html"
